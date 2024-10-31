8531b5cf3c5476288465cb8b6ef035c6a4b7cf0f
Crhystel <crhystelvelasco213@gmail.com>
2024-10-31 11:32:47 -0500

    Creación modelo ControlUsuario

---
+++ CCVProyectoP1/Models/ControlUsuario.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CCVProyecto1._1.Models
+{
+    public struct ControlUsuarios
+    {
+        public class Login
+        {
+            [Display(Name = "Nombre Usuario")]
+            public string? NombreUsuario { get; set; }
+            [Display(Name = "Contraseña")]
+            public string? Contrasenia { get; set; }
+
+        }
+    }
+}
