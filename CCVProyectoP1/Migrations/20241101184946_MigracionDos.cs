e4c86a7a2b951fbb477436044a41c22b7c563996
Crhystel <crhystelvelasco213@gmail.com>
2024-11-01 13:56:45 -0500

    Login de estudiante semi listo

---
+++ CCVProyectoP1/Migrations/20241101184946_MigracionDos.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CCVProyectoP1.Migrations
+{
+    /// <inheritdoc />
+    public partial class MigracionDos : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Estudiante",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Grado = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Cedula = table.Column<long>(type: "bigint", nullable: false),
+                    Nombre = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
+                    NombreUsuario = table.Column<string>(type: "nvarchar(150)", maxLength: 150, nullable: true),
+                    Contrasenia = table.Column<string>(type: "nvarchar(10)", maxLength: 10, nullable: true),
+                    Edad = table.Column<int>(type: "int", nullable: false),
+                    Rol = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Estudiante", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Estudiante");
+        }
+    }
+}
