52757a399ada523c4ecf923551e40049aae59ef6
Crhystel <crhystelvelasco213@gmail.com>
2024-11-01 13:23:32 -0500

    Login profesor listo

---
+++ CCVProyectoP1/Controllers/ProfesorContent.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CCVProyectoP1.Controllers
+{
+    public class ProfesorContent : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewData["MostrarSalir"] = true;
+            return View();
+        }
+        public async Task<IActionResult> Salir()
+        {
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
