7ebd4b0ae89b918e6c27be4eacbb97d0500459b9
Crhystel <crhystelvelasco213@gmail.com>
2024-11-01 12:58:35 -0500

    Login de vista profesor semi listo, creacion de nuevo controlador de loginprofe

--- CCVProyectoP1/Controllers/LoginProfeController.cs
+++ CCVProyectoP1/Controllers/LoginProfeController.cs
@@ -1,52 +1,46 @@
-﻿using Microsoft.AspNetCore.Mvc;
-
+﻿using CCVProyecto1._1.Models;
 using CCVProyectoP1.Data;
-using CCVProyecto1._1.Models;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
 using System.Security.Claims;
-
 
 namespace CCVProyectoP1.Controllers
 {
-    public class LoginController : Controller
+    public class LoginProfeController : Controller
     {
         private readonly Logica _logica;
-        public LoginController(Logica logica)
+        public LoginProfeController(Logica logica)
         {
             _logica = logica;
         }
+        
         public IActionResult Index()
         {
             return View();
         }
         [HttpPost]
-        public async Task<IActionResult> Index( Usuario _usuario)
+        public async Task<IActionResult> Index(Usuario _usuario)
         {
-            var usuario = await _logica.GetAdministradorsAsync(_usuario.NombreUsuario, _usuario.Contrasenia);
-            if(usuario != null)
+            var usuario = await _logica.GetProfesorAsync(_usuario.NombreUsuario, _usuario.Contrasenia);
+            if (usuario != null)
             {
                 var claims = new List<Claim>
                 {
-                    new Claim(ClaimTypes.Name,usuario.NombreUsuario),
+                    new Claim(ClaimTypes.Name, usuario.NombreUsuario),
                     new Claim("Contrasenia", usuario.Contrasenia),
                     new Claim(ClaimTypes.Role, usuario.Rol.ToString())
-                    //new Claim("Usuario", usuario.Usuario)
                 };
-             
-                var claimsIdentity = new ClaimsIdentity(claims,CookieAuthenticationDefaults.AuthenticationScheme);
-
+                var claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
                 await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(claimsIdentity));
-
-                return RedirectToAction("Index", "Administrador");
+                return RedirectToAction("Index", "ProfesorContent");
             }
             else
             {
                 ViewBag.ErrorMessage = "El nombre de usuario o la contraseña son incorrectos.";
                 return View();
             }
-          
-        }
 
+        }
     }
 }
