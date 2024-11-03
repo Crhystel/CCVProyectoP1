88f4bff77c0a2ca1cea9c0db1e64efc014b51d8e
Sol8888 <daniela.cabrera.tapia@udla.edu.ec>
2024-11-03 16:00:21 -0500

    llamar al controlador clases desde index administrador

--- CCVProyectoP1/Controllers/Controlador CRUD/AdministradorController.cs
+++ CCVProyectoP1/Controllers/Controlador CRUD/AdministradorController.cs
@@ -15,13 +15,6 @@ namespace CCVProyectoP1.Controllers
         public IActionResult Index()
         {
             ViewData["MostrarSalir"] = true;
-            var clases = _context.Clase.ToList();
-            var profesores = _context.Profesor.ToList();
-            var estudiantes = _context.Estudiante.ToList();
-
-            ViewBag.Clases = clases;
-            ViewBag.Profesores = profesores;
-            ViewBag.Estudiantes = estudiantes;
             return View();
         }
         public async Task<IActionResult> Salir()
@@ -29,39 +22,7 @@ namespace CCVProyectoP1.Controllers
             await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
             return RedirectToAction("Index", "Home"); 
         }
-        private readonly CCVProyectoP1Context _context;
-
-        public AdministradorController(CCVProyectoP1Context context)
-        {
-            _context = context;
-        }
-        public IActionResult ListaClases()
-        {
-            var clases = _context.Clase.ToList();
-            return View(clases);
-        }
-        public IActionResult Crete()
-        {
-            return View();
-        }
-
        
-
-        public IActionResult AsignarProfesorAClase(int claseId, int profesorId)
-        {
-            var clase = _context.Clase.Find(claseId);
-            clase.IdProfesor = profesorId;
-            _context.SaveChanges();
-            return RedirectToAction("Index");
-        }
-
-        public IActionResult AsignarEstudiantesAClase(int claseId, List<int> estudianteIds)
-        {
-            var clase = _context.Clase.Include(c => c.Estudiante).FirstOrDefault(c => c.Id == claseId);
-            clase.Estudiante = _context.Estudiante.Where(e => estudianteIds.Contains(e.Id)).ToList();
-            _context.SaveChanges();
-            return RedirectToAction("Index");
-        }
     }
 
 
