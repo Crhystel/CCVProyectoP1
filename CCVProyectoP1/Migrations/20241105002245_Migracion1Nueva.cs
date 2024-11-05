86bd4da918c34b7b889e1ae9cff37381ae7497fa
Crhystel <crhystelvelasco213@gmail.com>
2024-11-04 21:27:16 -0500

    No tengo idea de que paso

---
+++ CCVProyectoP1/Migrations/20241105002245_Migracion1Nueva.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CCVProyectoP1.Migrations
+{
+    /// <inheritdoc />
+    public partial class Migracion1Nueva : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Administrador",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Cedula = table.Column<string>(type: "nvarchar(10)", maxLength: 10, nullable: false),
+                    Nombre = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    NombreUsuario = table.Column<string>(type: "nvarchar(150)", maxLength: 150, nullable: false),
+                    Contrasenia = table.Column<string>(type: "nvarchar(10)", maxLength: 10, nullable: false),
+                    Edad = table.Column<int>(type: "int", nullable: false),
+                    Rol = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Administrador", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Estudiante",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Grado = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Cedula = table.Column<string>(type: "nvarchar(10)", maxLength: 10, nullable: false),
+                    Nombre = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    NombreUsuario = table.Column<string>(type: "nvarchar(150)", maxLength: 150, nullable: false),
+                    Contrasenia = table.Column<string>(type: "nvarchar(10)", maxLength: 10, nullable: false),
+                    Edad = table.Column<int>(type: "int", nullable: false),
+                    Rol = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Estudiante", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Profesor",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Materia = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Cedula = table.Column<string>(type: "nvarchar(10)", maxLength: 10, nullable: false),
+                    Nombre = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    NombreUsuario = table.Column<string>(type: "nvarchar(150)", maxLength: 150, nullable: false),
+                    Contrasenia = table.Column<string>(type: "nvarchar(10)", maxLength: 10, nullable: false),
+                    Edad = table.Column<int>(type: "int", nullable: false),
+                    Rol = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Profesor", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Clase",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    IdProfesor = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Clase", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Clase_Profesor_IdProfesor",
+                        column: x => x.IdProfesor,
+                        principalTable: "Profesor",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Actividad",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Titulo = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Descripcion = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    FechaCreacion = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    FechaEntrega = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ClaseId = table.Column<int>(type: "int", nullable: false),
+                    NombreArchivo = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    RutaArchivo = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Actividad", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Actividad_Clase_ClaseId",
+                        column: x => x.ClaseId,
+                        principalTable: "Clase",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ClaseEstudiante",
+                columns: table => new
+                {
+                    ClaseId = table.Column<int>(type: "int", nullable: false),
+                    EstudianteId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ClaseEstudiante", x => new { x.ClaseId, x.EstudianteId });
+                    table.ForeignKey(
+                        name: "FK_ClaseEstudiante_Clase_ClaseId",
+                        column: x => x.ClaseId,
+                        principalTable: "Clase",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ClaseEstudiante_Estudiante_EstudianteId",
+                        column: x => x.EstudianteId,
+                        principalTable: "Estudiante",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Administrador",
+                columns: new[] { "Id", "Cedula", "Contrasenia", "Edad", "Nombre", "NombreUsuario", "Rol" },
+                values: new object[] { 1, "1234567890", "admin", 30, "Roberto", "admin", 0 });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Actividad_ClaseId",
+                table: "Actividad",
+                column: "ClaseId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Clase_IdProfesor",
+                table: "Clase",
+                column: "IdProfesor");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ClaseEstudiante_EstudianteId",
+                table: "ClaseEstudiante",
+                column: "EstudianteId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Actividad");
+
+            migrationBuilder.DropTable(
+                name: "Administrador");
+
+            migrationBuilder.DropTable(
+                name: "ClaseEstudiante");
+
+            migrationBuilder.DropTable(
+                name: "Clase");
+
+            migrationBuilder.DropTable(
+                name: "Estudiante");
+
+            migrationBuilder.DropTable(
+                name: "Profesor");
+        }
+    }
+}
